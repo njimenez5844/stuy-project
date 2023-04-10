20453d93e27b15e97e90bea530df7c7f5da780ba
yaebk <87430416+yaebk@users.noreply.github.com>
2023-04-10 18:30:22 -0400

    code move
    making message a local variable

--- flask.py
+++ flask.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import online_test_method
 
 app = Flask(__name__)
-messages = []
 
 # input machine learning algorithm here 
 # bot = pipeline('text-generation', model='model_name')
@@ -13,6 +12,7 @@ def home():
 
 @app.route('/send', methods=['POST'])
 def send():
+    messages = []
     message = request.form['message']
     messages.append(('user', message))
     # bot's response generated from our algorithm 
