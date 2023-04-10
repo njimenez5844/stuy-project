2b632d58e82c5a6e91cee3522e5620ab3dfbd187
yaebk <87430416+yaebk@users.noreply.github.com>
2023-04-10 18:27:20 -0400

    Update FLASK.py
    fixing path again

--- FLASK.py
+++ FLASK.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import online_test_method
 
-app = Flask(__name__, template_folder= '.')
+app = Flask(__name__)
 messages = []
 
 # input machine learning algorithm here 
