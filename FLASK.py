29196ed5a18a3a499f2719ae1da21539270452bd
yaebk <87430416+yaebk@users.noreply.github.com>
2023-04-10 14:00:15 -0400

    changing function call
    making it call our function instead of a stud function

--- FLASK.py
+++ FLASK.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, request
+from cmd_test_method import online_test
 
 app = Flask(__name__)
 messages = []
 
 # input machine learning algorithm here 
 # bot = pipeline('text-generation', model='model_name')
-
-# for testing purposes 
-def generate_bot_response(message):
-    return 'Hi, how can I help you?'
 
 @app.route('/')
 def home():
@@ -19,7 +16,7 @@ def send():
     message = request.form['message']
     messages.append(('user', message))
     # bot's response generated from our algorithm 
-    bot_response = generate_bot_response(message)
+    bot_response = online_test(message)
     messages.append(('bot', bot_response))
     return render_template('home.html', messages=messages)
 
