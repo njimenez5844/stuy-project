ed67cac5935c3947406f8bc35a933e1e63bee4ac
Richie_Xue <rxue40@stuy.edu>
2023-04-16 15:18:16 -0400

    rename to app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', messages=messages)
+    return render_template('home.html', messages=[])
 
 @app.route('/send', methods=['POST'])
 def send():
