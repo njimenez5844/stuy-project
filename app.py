babf030ec94274c157c63a685fb83b20162ca666
Keobkeig <angela.xue3@gmail.com>
2023-06-12 14:42:55 -0400

    History now saves per session

--- app.py
+++ app.py
@@ -13,36 +13,37 @@ def home():
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
+    global history_df
     if request.method == 'POST':
-        username = request.form['username']
-        question = request.form['message']
-        label = request.form['label']
-
-        global history_df
-        history_df = history_df.append({'Username': username, 'Question': question, 'Label': label},
-                                       ignore_index=True)
-        history_df.to_csv('data/history.csv', index=False)  # Save the dataframe to CSV
-
         return redirect('/history')
-
     return render_template('home.html', messages=[])
-
 
 @app.route('/history')
 def history():
-    global history_df
     form_history = history_df.to_dict('records')
     return render_template('history.html', form_history=form_history)
 
 
 @app.route('/send', methods=['POST'])
 def send():
+    #username = request.form['username']
+    question = request.form['message']
+    label = request.form['label']
+
     messages = []
     message = request.form['message']
     messages.append(('user', message))
+
     # bot's response generated from our algorithm
     bot_response = online_test_method.online_test(message)
     messages.append(('bot', bot_response))
+      
+    history_df = history_df.append({'Username': 'keobkeig', 'Question': question, 'Label': label},
+                                    ignore_index=True)
+    
+    #To save the history of the chat in memory onto a csv file
+    #history_df.to_csv('data/history.csv', index=False)  # Save the dataframe to CSV
+
     return render_template('response.html', messages=messages)
 
 
